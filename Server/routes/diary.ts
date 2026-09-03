7fed89567ddfb32d7f0a5761bb9c19a913b0391f
Wajdaan Malik <1wajdaanmalik@gmail.com>
2026-09-03 18:11:48 -0400

    added auth and try catch to diary

--- Server/routes/diary.ts
+++ Server/routes/diary.ts
@@ -1,39 +1,43 @@
 import { Router, type Request, type Response } from 'express'
 import { prisma } from '../lib/prisma.ts';
+import { authMiddleware } from '../middlewares/authMiddleware.ts';
 
 const router = Router()
 
 //make a diary entry
-router.post('/diary', async (req: Request, res: Response) => {
-    const { user_id, diary_id, date } = req.body
+router.post('/diary', authMiddleware, async (req: Request, res: Response) => {
+    const { user_id, date } = req.body
+    try{
     const result = await prisma.diary.create({
         data: {
             user_id: user_id,
             date: new Date(date)
         }
     })
-    if (!result) {
-        res.status(500).json({error: 'Failed to create diary entry'})
-    } else {
-        res.status(201).json({message: 'Diary entry created successfully', data: result})
-    }
+    return res.status(201).json({message: 'Diary entry created', data: result})
+} catch (error) {
+    console.error(error)
+    res.status(500).json({error: 'Failed to create diary entry', details: error})
+    return
+}
 })
 
 //get diary entry by date
-router.get('/diary', async (req: Request, res: Response) => {
+router.get('/diary', authMiddleware, async (req: Request, res: Response) => {
     const {user_id, date} = req.query
+    try{
     const result  = await prisma.diary.findFirst({
         where: {
             user_id: String(user_id),
             date: new Date(String(date))
         }
     })  
-    if (!result) {
-        res.status(404).json({error: 'Diary entry not found'})
-    } else {
-        res.status(200).json({message: 'Diary entry found', data: result})
+    return res.status(200).json({message: 'Diary entry found', data: result})
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({error: 'Failed to get diary entry', details: error})
+        return
     }
-
 })
 
 export default router
